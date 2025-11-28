5bde6e9c54492d5f452ac83a4bfcfd3c5745fa4a
krowbee <skoresplay@gmail.com>
2025-11-28 13:52:33 +0200

    feat:created TokenService, verify and generate token

--- todo-app-test-backend/src/app.ts
+++ todo-app-test-backend/src/app.ts
@@ -1,8 +1,14 @@
 import { Response, Request } from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const express = require("express");
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World@@");
