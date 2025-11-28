4d6609f4bb0a954478322b700392f2c77be88858
krowbee <skoresplay@gmail.com>
2025-11-28 12:20:19 +0200

    Project initialized, prisma connected, and db designed

---
+++ todo-app-test-backend/src/app.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from "express";
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World@@");
+});
+
+app.listen(port, () => {
+  console.log(`Server works on port ${port} `);
+});
