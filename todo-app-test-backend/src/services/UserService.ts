175113ad33a3f0ee13a1a313cdb2117b9b248f4f
krowbee <skoresplay@gmail.com>
2025-11-28 13:34:16 +0200

    created zod schemas for validation, middleware for validation, and auth logic in UserService

--- todo-app-test-backend/src/services/UserService.ts
+++ todo-app-test-backend/src/services/UserService.ts
@@ -1,12 +1,26 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 import { prisma } from "../prisma/prisma";
+import * as bcrypt from "bcrypt";
+
+const saltOrRounds = 12;
 
 export class UserService {
-  async createUser(userCreateInput: Prisma.UserCreateInput) {
-    return prisma.user.create({ data: userCreateInput });
+  async registerUser(email: string, password: string) {
+    const isUser = await this.findUserByEmail(email);
+    if (isUser) throw new Error("User already exist");
+    const passwordHash = await bcrypt.hash(password, saltOrRounds);
+    return prisma.user.create({ data: { email, password: passwordHash } });
   }
 
-  async findUser(userWhereUniqueInput: Prisma.UserWhereUniqueInput) {
-    return prisma.user.findUnique({ where: userWhereUniqueInput });
+  async loginUser(email: string, password: string): Promise<User> {
+    const isUser = await this.findUserByEmail(email);
+    if (!isUser) throw new Error("User is not exist");
+    const isCorrectPassword = await bcrypt.compare(password, isUser.password);
+    if (!isCorrectPassword) throw new Error("Incorrect password");
+    return isUser;
+  }
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { email } });
   }
 }
