3c158fc95543505fcf00166ba9d19fb341c043b6
krowbee <skoresplay@gmail.com>
2025-11-28 15:52:30 +0200

    feat: task service completed

--- todo-app-test-backend/src/services/TaskService.ts
+++ todo-app-test-backend/src/services/TaskService.ts
@@ -1,15 +1,24 @@
+import { HttpException } from "../errors/HttpException";
 import { prisma } from "../prisma/prisma";
 import { Prisma, Task } from "@prisma/client";
 
+enum TaskStatus {
+  TODO = "TODO",
+  PROGRESS = "PROGRESS",
+  COMPLETE = "COMPLETE",
+}
+
 export class TaskService {
-  async findTask(
+  async findTaskById(
     taskWhereUniqueInput: Prisma.TaskWhereUniqueInput
-  ): Promise<Task | null> {
-    return prisma.task.findUnique({ where: taskWhereUniqueInput });
+  ): Promise<Task> {
+    const task = await prisma.task.findUnique({ where: taskWhereUniqueInput });
+    if (!task) throw new HttpException(404, "Incorrect task id");
+    return task;
   }
 
-  async findTasks(taskWhereInput?: Prisma.TaskWhereInput): Promise<Task[]> {
-    return prisma.task.findMany({ where: taskWhereInput });
+  async findUserTasks(userId: number, status?: TaskStatus): Promise<Task[]> {
+    return prisma.task.findMany({ where: { ownerId: userId, status } });
   }
 
   async createTask(taskCreateInput: Prisma.TaskCreateInput): Promise<Task> {
@@ -30,5 +39,14 @@ export class TaskService {
     taskWhereUniqueInput: Prisma.TaskWhereUniqueInput
   ): Promise<Task> {
     return prisma.task.delete({ where: taskWhereUniqueInput });
+  }
+
+  async validateAccess(taskId: number, userId: number): Promise<boolean> {
+    const task = await this.findTaskById({ id: taskId });
+
+    if (task.ownerId !== userId)
+      throw new HttpException(403, "You don't have access to this resource");
+
+    return true;
   }
 }
